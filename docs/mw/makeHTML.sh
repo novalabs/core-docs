c4715fb0e965f31f1820633b9273672fc19734be
Davide Rizzi <d.rizzi@novalabs.io>
2016-04-13 17:00:01 +0200

    First (TEST) commit

---
+++ docs/mw/makeHTML.sh
@@ -0,0 +1,1 @@
+asciidoctor -r asciidoctor-diagram -a icons=font -a source-highlighter=coderay index.ad
---
+++ docs/mw/makePDF.sh
@@ -0,0 +1,1 @@
+asciidoctor -r asciidoctor-pdf -r asciidoctor-diagram -a icons=font -a source-highlighter=coderay -b pdf index.ad
