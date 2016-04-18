48b10ed68a524f8869f9e4ba45847b22f175eb51
Davide Rizzi <d.rizzi@novalabs.io>
2016-04-18 15:22:08 +0200

    Removed useless and outdated .sh

--- docs/mw/makeHTML.sh
+++
@@ -1,1 +0,0 @@
-asciidoctor -r asciidoctor-diagram -a icons=font -a source-highlighter=coderay index.ad
--- docs/mw/makePDF.sh
+++
@@ -1,1 +0,0 @@
-asciidoctor -r asciidoctor-pdf -r asciidoctor-diagram -a icons=font -a source-highlighter=coderay -b pdf index.ad
